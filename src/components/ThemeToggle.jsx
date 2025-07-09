51598e9e568f053d30a2b6bb247301b6946310ef
Hong CS <114923987+Tamdeii@users.noreply.github.com>
2025-07-09 00:09:42 -0500

    Update toggle theme

--- src/components/ThemeToggle.jsx
+++ src/components/ThemeToggle.jsx
@@ -34,13 +34,14 @@ export const ThemeToggle = () => {
             onClick={toggleTheme}
             className={cn(
                 "fixed max-sm:hidden top-5 right-5 z-50 p-2 rounded-full transition-colors duration-300",
-                "focus:outline-none"
+                "focus:outline-hidden"
             )}
         >
             {isDarkMode ? (
-                <Sun className="h-6 w-6 text-yellow-300" />
+                <Sun className="h-6 w-6 text-yellow-300 -mt-1.5" />
             ) : (
-                <Moon className="h-6 w-6 text-blue-900 mt-1" />
+                <Moon className="h-6 w-6 text-blue-900 -mt-1.5" />
+
             )}
         </button>
     );
