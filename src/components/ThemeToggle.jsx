b47acaf9dc4023fa72d7712ed134f5358f10eea7
Hong CS <114923987+Tamdeii@users.noreply.github.com>
2025-07-08 23:42:38 -0500

    update theme toggle

--- src/components/ThemeToggle.jsx
+++ src/components/ThemeToggle.jsx
@@ -1,13 +1,13 @@
-import {useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Sun, Moon } from 'lucide-react';
 import { cn } from '@/lib/utils';
 
 export const ThemeToggle = () => {
-    const [isdarkMode, setIsDarkMode] = useState(false);
+    const [isDarkMode, setIsDarkMode] = useState(false);
 
     useEffect(() => {
         const storedTheme = localStorage.getItem("theme");
-        if (storedTheme === "dark"){
+        if (storedTheme === "dark") {
             setIsDarkMode(true);
             document.documentElement.classList.add("dark");
         }else{
@@ -18,7 +18,7 @@ export const ThemeToggle = () => {
     }, []);
 
     const toggleTheme = () => {
-        if(isdarkMode){
+        if (isDarkMode) {
             document.documentElement.classList.remove("dark");
             localStorage.setItem("theme", "light");
             setIsDarkMode(false);
@@ -30,18 +30,18 @@ export const ThemeToggle = () => {
     };
 
     return (
-    <button 
-        onClick={toggleTheme} 
-        className={cn(
-            "fixed max-sm:hidden top-5 right-5 z-50 p-2 rounded-full transition-colors duration-300",
-            "focus:outlin-hidden"
-    )}
-    >
-        {isdarkMode ? (
-            <Sun className = "h-6 w-6 text-yellow-300" />
-        ) : (
-            <Moon className= "h-6 w-6 text-blue-900"/>
-        )}      
-    </button>
+        <button
+            onClick={toggleTheme}
+            className={cn(
+                "fixed max-sm:hidden top-5 right-5 z-50 p-2 rounded-full transition-colors duration-300",
+                "focus:outline-none"
+            )}
+        >
+            {isDarkMode ? (
+                <Sun className="h-6 w-6 text-yellow-300" />
+            ) : (
+                <Moon className="h-6 w-6 text-blue-900 mt-1" />
+            )}
+        </button>
     );
-} 
+};
